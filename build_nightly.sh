be82c43ee918e876d0f5fc3dc59a84ebd5fca0e6
dtrail <mnl.manz@gmail.com>
2013-08-08 16:00:09 +0200

    Fix up Nightly build script for new wroking server

--- build_nightly.sh
+++ build_nightly.sh
@@ -22,17 +22,17 @@ lunch 12
 # built kernel & modules
 echo "Building modules..."
 echo " "
-make -j8 TARGET_KERNEL_SOURCE=/home/mnl-manz/razr_kdev_kernel/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig CONFIG_APANIC_PLABEL="mmcblk1p20" $OUT/boot.img
+make -j8 TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig CONFIG_APANIC_PLABEL="mmcblk1p20" $OUT/boot.img
 
 # We don't use the kernel but the modules
 echo "Copying modules to package folder"
 echo " "
-cp -r /home/mnl-manz/android/system/out/target/product/spyder/system/lib/modules/* /home/mnl-manz/razr_kdev_kernel/built/nightly/system/lib/modules/
+cp -r /home/dtrail/android/system/out/target/product/spyder/system/lib/modules/* /home/dtrail/android/built/nightly/system/lib/modules/
 
 # Switch to kernel folder
 echo "Entering kernel source..."
 echo " "
-cd ~/razr_kdev_kernel/android_kernel_motorola_omap4-common
+cd ~/android/android_kernel_motorola_omap4-common
 
 # Exporting the toolchain (You may change this to your local toolchain location)
 echo "Starting configuration of kernel..."
@@ -61,10 +61,10 @@ make -j4
 echo "Packaging flashable Zip file..."
 echo " "
 echo " "
-cp arch/arm/boot/zImage /home/mnl-manz/razr_kdev_kernel/built/nightly/system/etc/kexec/kernel
+cp arch/arm/boot/zImage /home/dtrail/android/built/nightly/system/etc/kexec/kernel
 
-cd /home/mnl-manz/razr_kdev_kernel/built/nightly
+cd /home/dtrail/android/built/nightly
 zip -r "JBX-Kernel-0.7b-Hybrid-Nightly_$(date +"%Y-%m-%d").zip" *
-mv "JBX-Kernel-0.7b-Hybrid-Nightly_$(date +"%Y-%m-%d").zip" /home/mnl-manz/razr_kdev_kernel/built
+mv "JBX-Kernel-0.7b-Hybrid-Nightly_$(date +"%Y-%m-%d").zip" /home/dtrail/android/built
 
 echo "done"
