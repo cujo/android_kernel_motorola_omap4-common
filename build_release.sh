8fb30e9059dee306bd639ee62f5ce72078fc7cc8
dtrail <mnl.manz@gmail.com>
2013-11-18 17:53:34 +0100

    build script: fixes

--- build_release.sh
+++ build_release.sh
@@ -22,60 +22,40 @@ croot
 lunch cm_spyder-eng
 
 # built kernel & modules
-echo "Building modules..."
+echo "Building kernel and modules..."
 echo " "
-make -j8 TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig CONFIG_APANIC_PLABEL="mmcblk1p20" $OUT/boot.img
+export ARCH=arm
+export SUBARCH=arm
+export LOCALVERSION="-JBX-0.9.0-Hybrid-4.2"
+make -j4 TARGET_KERNEL_SOURCE=/home/dtrail/android/android_kernel_motorola_omap4-common/ TARGET_KERNEL_CONFIG=mapphone_OCE_defconfig $OUT/boot.img
 
-# We don't use the kernel but the modules
 echo "Copying modules to package folder"
 echo " "
 cp -r /home/dtrail/android/4.2/out/target/product/spyder/system/lib/modules/* /home/dtrail/android/built/rls/system/lib/modules/
+cp /home/dtrail/android/4.2/out/target/product/spyder/kernel /home/dtrail/android/built/rls/system/etc/kexec/
 
-# Switch to kernel folder
-echo "Entering kernel source..."
+echo "------------- "
+echo "Done building"
+echo "------------- "
 echo " "
-cd ~/android/android_kernel_motorola_omap4-common
-
-# Exporting the toolchain (You may change this to your local toolchain location)
-echo "Starting configuration of kernel..."
-echo " "
-export PATH=~/android/system/prebuilt/linux-x86/toolchain/arm-eabi-4.4.3/bin:$PATH
-
-# export the flags (leave this alone)
-export ARCH=arm
-export SUBARCH=arm
-export CROSS_COMPILE=arm-eabi-
-
-# define the defconfig (Do not change)
-make ARCH=arm mapphone_OCE_defconfig
-export LOCALVERSION="-JBX-0.9.0-Hybrid-4.2"
-
-# execute build command with "-j4 core flag" 
-# (You may change this to the count of your CPU.
-# Don't set it too high or it will result in a non-bootable kernel.
-echo "Building..."
-echo " "
-make -j4
 
 # Copy and rename the zImage into nightly/nightly package folder
 # Keep in mind that we assume that the modules were already built and are in place
 # So we just copy and rename, then pack to zip including the date
 echo "Packaging flashable Zip file..."
 echo " "
-cp arch/arm/boot/zImage /home/dtrail/android/built/rls/system/etc/kexec/kernel
 
 cd /home/dtrail/android/built/rls
 zip -r "JBX-Kernel-0.9.0-Hybrid-4.2_$(date +"%Y-%m-%d").zip" *
 mv "JBX-Kernel-0.9.0-Hybrid-4.2_$(date +"%Y-%m-%d").zip" /home/dtrail/android/out
 
 # Exporting changelog to file
-echo "Exporting changelog to file: '/built/Changelog-[date]'"
-echo " "
 cd /home/dtrail/android/android_kernel_motorola_omap4-common
-git log --oneline --since="4 day ago" > /home/dtrail/android/android_kernel_motorola_omap4-common/changelog/Changelog_$(date +"%Y-%m-%d")
-git log --oneline  > /home/dtrail/android/android_kernel_motorola_omap4-common/changelog/Full_History_Changelog
-git add changelog/ .
-git commit -m "Added todays changelog and updated full history"
-git push origin JBX_STABLE
-
-echo "done"
+while true; do
+    read -p "Do you wish to push the latest changelog?" yn
+    case $yn in
+        [Yy]* ) echo "Exporting changelog to file: '/built/Changelog-[date]'"; echo " "; git log --oneline --since="4 day ago" > /home/dtrail/android/android_kernel_motorola_omap4-common/changelog/Changelog_$(date +"%Y-%m-%d"); git log --oneline  > /home/dtrail/android/android_kernel_motorola_omap4-common/changelog/Full_History_Changelog; git add changelog/ .; git commit -m "Added todays changelog and updated full history"; git push origin JBX_STABLE; echo " "; echo "done"; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
