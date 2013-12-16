dd3271431975c0c3eae776dfad04c559e4010c58
dtrail <mnl.manz@gmail.com>
2013-12-16 16:43:07 +0100

    mm: fault: Fix missing entry

--- arch/arm/mm/fault.c
+++ arch/arm/mm/fault.c
@@ -341,6 +341,7 @@ fault = __do_page_fault(mm, addr, fsr, flags, tsk);
       /* Clear FAULT_FLAG_ALLOW_RETRY to avoid any risk
       * of starvation. */
       flags &= ~FAULT_FLAG_ALLOW_RETRY;
+      flags |= FAULT_FLAG_TRIED;
       goto retry;
     }
   }
