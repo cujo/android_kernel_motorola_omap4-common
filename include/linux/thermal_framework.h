54b38da1fde9ac145c57e121422798f321d80e04
dtrail <mnl.manz@gmail.com>
2014-03-23 23:46:05 +0100

    thermal_framework: add missing struct *domain

--- include/linux/thermal_framework.h
+++ include/linux/thermal_framework.h
@@ -19,7 +19,6 @@
 #define __LINUX_THERMAL_FRAMEWORK_H__
 
 struct thermal_dev;
-struct thermal_domain;
 
 /**
  * struct thermal_dev_ops  - Structure for device operation call backs
@@ -68,6 +67,7 @@ struct thermal_dev {
 	struct list_head node;
 	int		index;
 	int 		current_temp;
+	struct thermal_domain   *domain;
 
 };
 
